d3551c465d80d83796e8169306b94c2ea922a6a9
Varun Ramakrishnan <ravarun3@gmail.com>
2023-12-22 22:19:58 +0530

    Solve Day 8

--- day8/day8.py
+++ day8/day8.py
@@ -1,4 +1,5 @@
 import re
+from math import lcm
 
 data = open("input.txt", "r").read().strip().split("\n")
 
@@ -8,8 +9,8 @@ def parse_input(data: list[str]):
     map = {}
     for line in data[2:]:
         start = line.split("=")[0].strip()
-        l = re.findall(r"(?<=\()[A-Z]+(?=,)", line)[0]
-        r = re.findall(r"(?<= )[A-Z]+(?=\))", line)[0]
+        l = re.findall(r"(?<=\()\w+(?=,)", line)[0]
+        r = re.findall(r"(?<= )\w+(?=\))", line)[0]
         map[start] = (l, r)
 
     return inst, map
@@ -21,7 +22,6 @@ inst_to_index = {
 }
 
 def part_one(data: list[str]):
-
     inst, map = parse_input(data)
 
     curr = "AAA"
@@ -40,7 +40,31 @@ def part_one(data: list[str]):
 
 
 def part_two(data: list[str]):
-    ...
+    inst, map = parse_input(data)
+
+    currs = list(filter(lambda x: x[-1] == "A", map.keys()))
+    inst_index = 0
+    steps = 0
+
+    cycle_lengths = [-1 for _ in range(len(currs))]
+
+    while len(list(filter(lambda x: x == -1, cycle_lengths))):
+        next = []
+        curr_inst = inst[inst_index]
+        inst_index = (inst_index + 1) % len(inst)
+
+        for index, curr in enumerate(currs):
+            if curr[-1] == "Z":
+                cycle_lengths[index] = steps
+
+            next.append(map[curr][inst_to_index[curr_inst]])
+        steps += 1
+
+        currs = next
+
+    return lcm(*cycle_lengths)
+
+
 
 print(part_one(data))
 print(part_two(data))
